e1b66ee9fb9e9a00c9d928b2b911d5f534014c6b
alejo <alejo12arias@gmail.com>
2023-11-14 00:55:11 -0300

    Proyecto terminado ultima version

--- Proyecto_DW/js/conversion.js
+++ Proyecto_DW/js/conversion.js
@@ -1,8 +1,7 @@
 const botonConvertir = document.getElementById("botonConvertir");
-
 botonConvertir.addEventListener('click', function() {
 
-    const numeroIngresado = parseInt(document.getElementById("numeroIngresado").value);
+    const numeroIngresado = parseFloat(document.getElementById("numeroIngresado").value);
     const verifNumIngreso = document.getElementById("numeroIngresado").value;
     const op1 = document.getElementById("op1").value;
     const op2 = document.getElementById("op2").value;
@@ -10,95 +9,100 @@ botonConvertir.addEventListener('click', function() {
 
     let numConv; 
 
-    if (verifNumIngreso !== "") {
+    if (verifNumIngreso !== "" ) {
 
-        switch (op1) {
+        if (verifNumIngreso.includes("-")) {
+            mostrar.value="No puede ingresar negativos!"
+            
+        } else {
+            switch (op1) {
         
-            //CONVIERTE DE SEGUNDO A LOS DEMAS.
-            case "s1":
-                if (op2 === "s2") {
-                    mostrar.value = "No se puede convertir de Seg a Seg"
-    
-                } else if (op2 === "m2") {
-                    numConv = numeroIngresado/60;
-                    mostrar.value = numConv + " minutos";
-                
-                } else if (op2 === "h2") {
-                    numConv = numeroIngresado/3600;
-                    mostrar.value = numConv + " hora/s";
-                
-                } else if (op2 === "d2") {
-                    numConv = numeroIngresado/86400;
-                    mostrar.value = numConv + " dias";
-                }
-                break;
-    
-            //CONVIERTE DE MINUTOS A LOS DEMAS
-            case "m1":
-    
-                if (op2 === "s2") {
-                    numConv = numeroIngresado * 60;
-                    mostrar.value = numConv + " Segundos"
-    
-                } else if (op2 === "m2") {
-                    mostrar.value = "No se puede convertir de Min a Min"
-                
-                } else if (op2 === "h2") {
-                    numConv = numeroIngresado/60;
-                    mostrar.value = numConv + " hora/s";
-                
-                } else if (op2 === "d2") {
-                    numConv = numeroIngresado/1440;
-                    mostrar.value = numConv + " dia/s";
-                }
-    
-                break;
-    
-                //CONVIERTE DE HORAS A LOS DEMAS
-                case "h1":
-    
-                if (op2 === "s2") {
-                    numConv = numeroIngresado * 3600;
-                    mostrar.value = numConv + " Segundos"
-    
-                } else if (op2 === "m2") {
-                    numConv = numeroIngresado * 60;
-                    mostrar.value = numConv + " Minutos"
-                
-                } else if (op2 === "h2") {
-                    mostrar.value = numConv + "No se puede convertir de horas a Horas";
-                
-                } else if (op2 === "d2") {
-                    numConv = numeroIngresado/24;
-                    mostrar.value = numConv + " dia/s";
-                }
-    
-                break;
-    
-                //CONVIERTE DE DIAS A LOS DEMAS
-                case "d1":
-    
-                if (op2 === "s2") {
-                    numConv = numeroIngresado * 86400;
-                    mostrar.value = numConv + " Segundos"
-    
-                } else if (op2 === "m2") {
-                    numConv = numeroIngresado * 1440;
-                    mostrar.value = numConv + " Minutos"
-                
-                } else if (op2 === "h2") {
-                    numConv = numeroIngresado * 24;
-                    mostrar.value = numConv + " Hora/s";
-                
-                } else if (op2 === "d2") {
-                    mostrar.value = numConv + "No se puede convertir de Dias a Dias";
-                }
-    
-                break;
+                //CONVIERTE DE SEGUNDO A LOS DEMAS.
+                case "s1":
+                    if (op2 === "s2") {
+                        mostrar.value = "No se puede convertir de Seg a Seg"
         
-            default:
-                mostrar.value = "Debe de seleccionar una unidad :)"
-                break;
+                    } else if (op2 === "m2") {
+                        numConv = numeroIngresado/60;
+                        mostrar.value = numConv + " minutos";
+                    
+                    } else if (op2 === "h2") {
+                        numConv = numeroIngresado/3600;
+                        mostrar.value = numConv + " hora/s";
+                    
+                    } else if (op2 === "d2") {
+                        numConv = numeroIngresado/86400;
+                        mostrar.value = numConv + " dias";
+                    }
+                    break;
+        
+                //CONVIERTE DE MINUTOS A LOS DEMAS
+                case "m1":
+        
+                    if (op2 === "s2") {
+                        numConv = numeroIngresado * 60;
+                        mostrar.value = numConv + " Segundos"
+        
+                    } else if (op2 === "m2") {
+                        mostrar.value = "No se puede convertir de Min a Min"
+                    
+                    } else if (op2 === "h2") {
+                        numConv = numeroIngresado/60;
+                        mostrar.value = numConv + " hora/s";
+                    
+                    } else if (op2 === "d2") {
+                        numConv = numeroIngresado/1440;
+                        mostrar.value = numConv + " dia/s";
+                    }
+        
+                    break;
+        
+                    //CONVIERTE DE HORAS A LOS DEMAS
+                    case "h1":
+        
+                    if (op2 === "s2") {
+                        numConv = numeroIngresado * 3600;
+                        mostrar.value = numConv + " Segundos"
+        
+                    } else if (op2 === "m2") {
+                        numConv = numeroIngresado * 60;
+                        mostrar.value = numConv + " Minutos"
+                    
+                    } else if (op2 === "h2") {
+                        mostrar.value = numConv + "No se puede convertir de horas a Horas";
+                    
+                    } else if (op2 === "d2") {
+                        numConv = numeroIngresado/24;
+                        mostrar.value = numConv + " dia/s";
+                    }
+        
+                    break;
+        
+                    //CONVIERTE DE DIAS A LOS DEMAS
+                    case "d1":
+        
+                    if (op2 === "s2") {
+                        numConv = numeroIngresado * 86400;
+                        mostrar.value = numConv + " Segundos"
+        
+                    } else if (op2 === "m2") {
+                        numConv = numeroIngresado * 1440;
+                        mostrar.value = numConv + " Minutos"
+                    
+                    } else if (op2 === "h2") {
+                        numConv = numeroIngresado * 24;
+                        mostrar.value = numConv + " Hora/s";
+                    
+                    } else if (op2 === "d2") {
+                        mostrar.value = numConv + "No se puede convertir de Dias a Dias";
+                    }
+        
+                    break;
+            
+                default:
+                    mostrar.value = "Debe de seleccionar una unidad :)"
+                    break;
+            }
         }
         
     } else {
@@ -108,3 +112,16 @@ botonConvertir.addEventListener('click', function() {
     }
 
 });
+
+
+//BOTON PARA LIMPIAR LOS CAMPOS
+const clear = document.getElementById('botonClear')
+    clear.addEventListener('click', function() {
+
+        numeroIngresado.value = ""
+        op1.selectedIndex = 0
+        op2.selectedIndex = 0
+        mostrar.value = ""
+
+    })
+
